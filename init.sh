86a37473d89f5e5478dc4405d9080fb113e62391
zzzteph <stephanzag@gmail.com>
2022-11-24 22:43:01 +0100

    mysql docker

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 &
 nginx & docker-php-entrypoint php-fpm
