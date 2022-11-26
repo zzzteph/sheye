6569b4c1eb6e455b7052acd21f46ef0ca0512f50
zzzteph <stephanzag@gmail.com>
2022-11-26 16:22:30 +0100

    docker update

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+
+cd /var/www/html/ && php artisan key:generate
+cd /var/www/html/ && /var/www/html/php artisan migrate
+cd /var/www/html/ && /var/www/html/php artisan db:seed --class=ScannerSeeder
+cd /var/www/html/ && /var/www/html/php artisan add:user admin admin
 sh workers.sh & nginx & docker-php-entrypoint php-fpm 
